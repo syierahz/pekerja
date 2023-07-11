39c6e929db5cf5c96757a0df83572e86be78bdcb
Your Name <you@example.com>
2023-07-11 13:24:26 +0800

    tambah fail

--- info_pekerja.sql
+++ info_pekerja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2023 at 07:15 AM
+-- Generation Time: Jul 11, 2023 at 07:22 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
